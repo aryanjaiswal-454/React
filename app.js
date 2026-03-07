30c83010f20e7041e4d4b517dea5bda7bcd08db4
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-03-08 00:28:43 +0530

    igniting our app

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
-    "div",
-    {id:"parent"},
-    [
-        React.createElement(
-            "div",
-            {id:"child"},
-            [
-                React.createElement("h1",{},"I am a h1 tag"),
-                React.createElement("h2",{},"I am a h2 tag")
-            ]
-        ),
-        React.createElement(
-            "div",
-            {id:"child2"},
-            [
-                React.createElement("h1",{},"I am a h1 tag"),
-                React.createElement("h2",{},"I am a h2 tag")
-            ]
-        ),
-    ]   
-)
+  "div",
+  { id: "parent" },
+  [
+    React.createElement(
+      "div",
+      { id: "child", key: "child1" },
+      [
+        React.createElement("h1", { key: "h1" }, "I am a h1 tag"),
+        React.createElement("h2", { key: "h2" }, "yxskjdckvjceh")
+      ]
+    ),
+    React.createElement(
+      "div",
+      { id: "child2", key: "child2" },
+      [
+        React.createElement("h1", { key: "h1-2" }, "I am a h1 tag"),
+        React.createElement("h2", { key: "h2-2" }, "I am a h2 tag")
+      ]
+    )
+  ]
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
