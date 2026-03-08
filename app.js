0917f3bd2bc515900c39978a42ba4d42b72b0813
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-03-08 17:36:20 +0530

    jsx

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement(
-      "div",
-      { id: "child", key: "child1" },
-      [
-        React.createElement("h1", { key: "h1" }, "I am a h1 tag"),
-        React.createElement("h2", { key: "h2" }, "yxskjdckvjceh")
-      ]
-    ),
-    React.createElement(
-      "div",
-      { id: "child2", key: "child2" },
-      [
-        React.createElement("h1", { key: "h1-2" }, "I am a h1 tag"),
-        React.createElement("h2", { key: "h2-2" }, "I am a h2 tag")
-      ]
-    )
-  ]
-);
-
+const heading = React.createElement("h1",{id:"heading"},"Hello World");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
+console.log(heading)
+
+const jsxheading = (<h1 id="heading2"> Namaste React Heading</h1>);
+console.log(jsxheading)
+root.render(jsxheading)
