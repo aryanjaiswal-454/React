12c43114440aba8ca5f49de32e1d7a2210bf1b75
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-03-10 01:49:55 +0530

    dynamic data

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const Header = ()=>{
         </div>
     )
 }
-const RestaurantCard = ()=>{
+const RestaurantCard = (props)=>{
     return(
         <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
-            <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/8/431e7530-c1d2-46ff-911c-d8013b721041_1052897.jpg"/>
-            <h3>Meghana Foods</h3>
-            <h4>Pizza, North Indian, Asian</h4>
-            <h4>4.4 stars</h4>
-            <h4>38 minutes</h4>
+            <img className="res-logo" src={props.logo} />
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.stars} stars</h4>
+            <h4>{props.deliveryTime}</h4>
         </div>
     )
 }
@@ -33,26 +33,20 @@ const Body = ()=>{
         <div className="body">
             <div className="search">Search</div>
             <div className="res-container">
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
-                <RestaurantCard/>
+                <RestaurantCard 
+                    resName="Meghana Foods" 
+                    cuisine="Pizza, North Indian, Asian"
+                    stars="4.4"
+                    deliveryTime="38 minutes"
+                    logo="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2025/3/8/431e7530-c1d2-46ff-911c-d8013b721041_1052897.jpg"
+                />
+                <RestaurantCard 
+                    resName="KFC" 
+                    cuisine="Burgers, Fast Food"
+                    stars="4.2"
+                    deliveryTime="51 minutes"
+                    logo="https://imgs.search.brave.com/ap7jF-2e5-vlX1dlqv-UaCKVZJrspTZvcUeEF_PS1is/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/a2ZjLXN1aXNzZS5j/aC9maWxlYWRtaW4v/X3Byb2Nlc3NlZF8v/ZC85L2NzbV96aW5n/ZXJfZHVua2VkX2J1/cmdlcl9idW5kbGVf/ZHVua2VkX2hvdHdp/bmdzX2YxMTQyY2Jl/YjMucG5n"
+                />
             </div>
         </div>
     )
