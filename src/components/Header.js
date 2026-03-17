14cad5e021ef2015e92a439ebf9ab49b2d218a09
Aryan Jaiswal <45aryanjaiswal45@gmail.com>
2026-03-17 14:26:02 +0530

    cleaning code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,31 @@
 import { LOGO_URL } from "../utils/constants.js";
-import {useState, useEffect} from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
     const [btnName, setBtnName] = useState("Login");
     console.log("Header rendered");
-    useEffect(()=>{
-        console.log("useEffect called");
-    },[btnName])
     return (
         <div className="header">
             <div className="logo-container">
-                <img className="logo" src={LOGO_URL} />
+                <Link to="/">
+                    <img className="logo" src={LOGO_URL} />
+                </Link>
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact</li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
                     <button
                         className="login"
                         onClick={() => {
-                            btnName==="Login" ? setBtnName("Logout") : setBtnName("Login")
+                            btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
                         }}
                     >
                         {btnName}
